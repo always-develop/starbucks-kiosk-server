a3fc5a7d201d0b871d0e340d2c40402eef249038
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-22 23:25:03 +0900

    feat: initialize project

---
+++ src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/StarbucksKioskServerApplication.java
@@ -0,0 +1,13 @@
+package dev.kkkkkksssssaaaa.starbucks.kiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbucksKioskServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarbucksKioskServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/kkkkkksssssaaaa/starbucks/kiosk/StarbucksKioskServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kkkkkksssssaaaa.starbucks.kiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbucksKioskServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

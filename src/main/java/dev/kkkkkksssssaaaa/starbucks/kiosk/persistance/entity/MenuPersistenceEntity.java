7faa2d451e3f92789c6d14031fca093a327aee21
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-26 22:37:10 +0900

    feat: add menu type field to menu persistence entity

--- src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/persistance/entity/MenuPersistenceEntity.java
+++ src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/persistance/entity/MenuPersistenceEntity.java
@@ -1,6 +1,9 @@
 package dev.kkkkkksssssaaaa.starbucks.kiosk.persistance.entity;
 
+import dev.kkkkkksssssaaaa.starbucks.kiosk.domain.menu.data.MenuType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import lombok.Getter;
 
@@ -12,4 +15,7 @@ public class MenuPersistenceEntity extends BaseEntity {
     private String name;
 
     private int price;
+
+    @Enumerated(EnumType.STRING)
+    private MenuType type;
 }

35b81154dfb1aca15043147c296f9ce375813048
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-26 22:37:52 +0900

    feat: add menu persistence repository

---
+++ src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/persistance/repository/MenuPersistenceRepository.java
@@ -0,0 +1,14 @@
+package dev.kkkkkksssssaaaa.starbucks.kiosk.persistance.repository;
+
+import dev.kkkkkksssssaaaa.starbucks.kiosk.domain.menu.data.MenuType;
+import dev.kkkkkksssssaaaa.starbucks.kiosk.persistance.entity.MenuPersistenceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MenuPersistenceRepository extends JpaRepository<MenuPersistenceEntity, Long> {
+
+    List<MenuPersistenceEntity> findAllByType(MenuType type);
+}

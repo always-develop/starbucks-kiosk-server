239712754763054ecbf8cd56372b67b4dcc03a08
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-27 21:23:35 +0900

    refactor: modify menus response to menu response

---
+++ src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/domain/menu/dto/response/MenuResponse.java
@@ -0,0 +1,29 @@
+package dev.kkkkkksssssaaaa.starbucks.kiosk.domain.menu.dto.response;
+
+import dev.kkkkkksssssaaaa.starbucks.kiosk.domain.menu.dto.Menu;
+import lombok.Getter;
+
+@Getter
+public class MenuResponse {
+
+    private final Long id;
+    private final String name;
+    private final int price;
+    private final String image;
+
+    private MenuResponse(Long id, String name, int price, String image) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.image = image;
+    }
+
+    public static MenuResponse castDto(Menu menu) {
+        return new MenuResponse(
+            menu.getId(),
+            menu.getName(),
+            menu.getPrice(),
+            menu.getImage()
+        );
+    }
+}

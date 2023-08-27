9617152831ee302127c170c8abd3da4c95b61ab5
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-27 22:23:46 +0900

    feat: add member persistence entity

---
+++ src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/persistance/entity/MemberPersistenceEntity.java
@@ -0,0 +1,20 @@
+package dev.kkkkkksssssaaaa.starbucks.kiosk.persistance.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "member")
+public class MemberPersistenceEntity extends BaseEntity {
+
+    @Column(
+        unique = true,
+        length = 20,
+        nullable = false,
+        updatable = false
+    )
+    private String phone;
+}

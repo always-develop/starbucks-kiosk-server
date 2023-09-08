f3eefd64ff11ccc9700fb5817aae746e44e7e885
kkkkkksssssaaaa <kofc312@naver.com>
2023-09-09 00:18:28 +0900

    feat: add find membership query

---
+++ src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/persistance/repository/MembershipCustomRepositoryImpl.java
@@ -0,0 +1,54 @@
+package dev.kkkkkksssssaaaa.starbucks.kiosk.persistance.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import dev.kkkkkksssssaaaa.starbucks.kiosk.domain.membership.repository.dao.MembershipDao;
+import dev.kkkkkksssssaaaa.starbucks.kiosk.domain.membership.repository.dao.QMembershipDao;
+import dev.kkkkkksssssaaaa.starbucks.kiosk.persistance.entity.QMemberCouponPersistenceEntity;
+import dev.kkkkkksssssaaaa.starbucks.kiosk.persistance.entity.QMemberPersistenceEntity;
+import dev.kkkkkksssssaaaa.starbucks.kiosk.persistance.entity.QMemberStampPersistenceEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static com.querydsl.core.group.GroupBy.groupBy;
+import static com.querydsl.core.group.GroupBy.list;
+
+@Repository
+@Transactional(readOnly = true)
+public class MembershipCustomRepositoryImpl implements MembershipCustomRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Autowired
+    public MembershipCustomRepositoryImpl(JPAQueryFactory queryFactory) {
+        this.queryFactory = queryFactory;
+    }
+
+    @Override
+    public Optional<MembershipDao> findMembership(String phone) {
+        Map<String, MembershipDao> result = queryFactory
+            .from(QMemberPersistenceEntity.memberPersistenceEntity)
+            .where(QMemberPersistenceEntity.memberPersistenceEntity.phone.eq(phone))
+            .leftJoin(QMemberStampPersistenceEntity.memberStampPersistenceEntity)
+            .on(QMemberStampPersistenceEntity.memberStampPersistenceEntity.member.id.eq(QMemberPersistenceEntity.memberPersistenceEntity.id))
+            .leftJoin(QMemberCouponPersistenceEntity.memberCouponPersistenceEntity)
+            .on(QMemberCouponPersistenceEntity.memberCouponPersistenceEntity.member.id.eq(QMemberPersistenceEntity.memberPersistenceEntity.id))
+            .transform(
+                groupBy(QMemberPersistenceEntity.memberPersistenceEntity.phone)
+                    .as(
+                        new QMembershipDao(
+                            QMemberPersistenceEntity.memberPersistenceEntity,
+                            list(QMemberStampPersistenceEntity.memberStampPersistenceEntity),
+                            list(QMemberCouponPersistenceEntity.memberCouponPersistenceEntity)
+                        )
+                    )
+            );
+
+        return Optional.ofNullable(result.get(phone));
+    }
+}
+
+

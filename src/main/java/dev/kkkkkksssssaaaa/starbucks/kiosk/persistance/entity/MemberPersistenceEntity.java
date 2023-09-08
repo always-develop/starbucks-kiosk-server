d7267e5862a17c4e6c38dfbf3db68802bbfb2fce
kkkkkksssssaaaa <kofc312@naver.com>
2023-09-09 00:40:51 +0900

    feat: add constructor annotation to member persistence entity

--- src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/persistance/entity/MemberPersistenceEntity.java
+++ src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/persistance/entity/MemberPersistenceEntity.java
@@ -3,11 +3,15 @@ package dev.kkkkkksssssaaaa.starbucks.kiosk.persistance.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @Table(name = "member")
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberPersistenceEntity extends BaseEntity {
 
     @Column(
--- src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/persistance/repository/MemberPersistenceRepository.java
+++ src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/persistance/repository/MemberPersistenceRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberPersistenceRepository
-    extends JpaRepository<MemberPersistenceEntity, Long>, MembershipCustomRepository{
+    extends JpaRepository<MemberPersistenceEntity, Long>, MembershipCustomRepository {
 }

a89575cdcf00b2dd3a9c83b9b96a64fc060927de
kkkkkksssssaaaa <kofc312@naver.com>
2023-09-13 23:06:17 +0900

    feat: add member's phone to order domain's membership entity

--- src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/domain/order/dto/Membership.java
+++ src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/domain/order/dto/Membership.java
@@ -1,16 +1,24 @@
 package dev.kkkkkksssssaaaa.starbucks.kiosk.domain.order.dto;
 
+import dev.kkkkkksssssaaaa.starbucks.kiosk.domain.membership.dto.Phone;
+
 import java.util.Optional;
 
 public class Membership {
 
     private final Long id;
+    private final Phone phone;
 
-    public Membership(Long id) {
+    public Membership(Long id, Phone phone) {
         this.id = id;
+        this.phone = phone;
     }
 
     public boolean isGuest() {
         return Optional.ofNullable(id).isEmpty();
+    }
+
+    public Phone getPhone() {
+        return this.phone;
     }
 }

3f5d2d046eadc72edc5a9dfeaaf7b1af8016fce7
kkkkkksssssaaaa <kofc312@naver.com>
2023-09-13 23:07:12 +0900

    feat: add order number domain entity

---
+++ src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/domain/order/dto/OrderNumber.java
@@ -0,0 +1,22 @@
+package dev.kkkkkksssssaaaa.starbucks.kiosk.domain.order.dto;
+
+public class OrderNumber {
+
+    private final String value;
+
+    private OrderNumber(String value) {
+        this.value = value;
+    }
+
+    public static OrderNumber createByMembership(Membership membership) {
+        if (membership.isGuest()) {
+            return new OrderNumber(
+                String.valueOf(OrderNumberStore.issuedNewOrderNumber())
+            );
+        }
+
+        return new OrderNumber(
+            membership.getPhone().lastNumber()
+        );
+    }
+}

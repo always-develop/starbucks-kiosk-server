87aab79b646f6c6cb50269d0dfc7b928a042828a
kkkkkksssssaaaa <kofc312@naver.com>
2023-09-13 23:04:08 +0900

    feat: add order number store for guest order

---
+++ src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/domain/order/dto/OrderNumberStore.java
@@ -0,0 +1,16 @@
+package dev.kkkkkksssssaaaa.starbucks.kiosk.domain.order.dto;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class OrderNumberStore {
+
+    // TODO: reset to 100 every 0 o'clock
+    private static AtomicInteger orderNumber = new AtomicInteger(100);
+
+    private OrderNumberStore() {
+    }
+
+    public static int issuedNewOrderNumber() {
+        return orderNumber.incrementAndGet();
+    }
+}

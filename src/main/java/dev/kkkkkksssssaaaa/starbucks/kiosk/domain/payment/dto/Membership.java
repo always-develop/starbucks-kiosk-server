c76085deaf82506814791de2b255ceb2d518d40c
kkkkkksssssaaaa <kofc312@naver.com>
2023-10-09 23:31:27 +0900

    refactor: remove unused field to payment domain's membership entity

--- src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/domain/payment/dto/Membership.java
+++ src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/domain/payment/dto/Membership.java
@@ -1,24 +1,16 @@
 package dev.kkkkkksssssaaaa.starbucks.kiosk.domain.payment.dto;
 
-import dev.kkkkkksssssaaaa.starbucks.kiosk.domain.membership.dto.Phone;
-
 import java.util.Optional;
 
 public class Membership {
 
     private final Long id;
-    private final Phone phone;
 
-    public Membership(Long id, Phone phone) {
+    public Membership(Long id) {
         this.id = id;
-        this.phone = phone;
     }
 
     public boolean isGuest() {
         return Optional.ofNullable(id).isEmpty();
-    }
-
-    public Phone getPhone() {
-        return this.phone;
     }
 }

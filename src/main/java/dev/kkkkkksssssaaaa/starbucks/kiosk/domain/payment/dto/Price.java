0e94df8ca98110d91077bf1735ed06e7fcfb3865
kkkkkksssssaaaa <kofc312@naver.com>
2023-10-09 23:26:17 +0900

    feat: add price entity to payment domain

---
+++ src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/domain/payment/dto/Price.java
@@ -0,0 +1,37 @@
+package dev.kkkkkksssssaaaa.starbucks.kiosk.domain.payment.dto;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Price {
+
+    private static final int MIN = 0;
+    private final int price;
+
+    private Price(int price) {
+        if (price < MIN) {
+            log.warn("Price is must be more than 0.");
+            this.price = MIN;
+        } else {
+            this.price = price;
+        }
+    }
+
+    public static Price of(int fee) {
+        return new Price(fee);
+    }
+
+    public Price setDiscount(Coupon coupon) {
+        return new Price(
+            this.toInteger() - coupon.getDiscountPrice().toInteger()
+        );
+    }
+
+    public Price get() {
+        return this;
+    }
+
+    private int toInteger() {
+        return this.price;
+    }
+}

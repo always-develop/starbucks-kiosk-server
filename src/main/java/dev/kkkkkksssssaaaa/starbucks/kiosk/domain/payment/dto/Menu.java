55598397e1ebffb1c13fbe2baa87394ca8b98383
kkkkkksssssaaaa <kofc312@naver.com>
2023-10-09 23:26:45 +0900

    feat: add menu entity to payment domain

---
+++ src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/domain/payment/dto/Menu.java
@@ -0,0 +1,21 @@
+package dev.kkkkkksssssaaaa.starbucks.kiosk.domain.payment.dto;
+
+import dev.kkkkkksssssaaaa.starbucks.kiosk.persistance.entity.MenuPersistenceEntity;
+
+public class Menu {
+
+    private final Long id;
+    private final Price price;
+
+    private Menu(Long id, Price price) {
+        this.id = id;
+        this.price = price;
+    }
+
+    public static Menu castEntity(MenuPersistenceEntity entity) {
+        return new Menu(
+            entity.getId(),
+            Price.of(entity.getPrice())
+        );
+    }
+}

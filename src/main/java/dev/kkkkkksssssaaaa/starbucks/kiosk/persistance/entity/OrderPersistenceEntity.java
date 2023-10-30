c19ae0ddab00a879e71a2c96d824e43b5d9cb63b
kkkkkksssssaaaa <kofc312@naver.com>
2023-10-30 23:22:17 +0900

    feat: add fields to order persistence entity - status - total price

--- src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/persistance/entity/OrderPersistenceEntity.java
+++ src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/persistance/entity/OrderPersistenceEntity.java
@@ -1,6 +1,10 @@
 package dev.kkkkkksssssaaaa.starbucks.kiosk.persistance.entity;
 
+import dev.kkkkkksssssaaaa.starbucks.kiosk.persistance.entity.field.OrderStatus;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -17,4 +21,11 @@ public class OrderPersistenceEntity extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private MemberPersistenceEntity member;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private OrderStatus status;
+
+    @Column(name = "total_price")
+    private Integer totalPrice;
 }

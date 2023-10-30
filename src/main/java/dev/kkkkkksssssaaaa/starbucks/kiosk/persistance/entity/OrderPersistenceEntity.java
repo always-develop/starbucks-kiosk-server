c95b0664a563de06f9361852d95f53a0b54653e1
kkkkkksssssaaaa <kofc312@naver.com>
2023-10-30 23:22:36 +0900

    feat: add all arguments constructor to order persistence entity

--- src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/persistance/entity/OrderPersistenceEntity.java
+++ src/main/java/dev/kkkkkksssssaaaa/starbucks/kiosk/persistance/entity/OrderPersistenceEntity.java
@@ -9,6 +9,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,6 +17,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "order_history")
 @NoArgsConstructor
+@AllArgsConstructor
 public class OrderPersistenceEntity extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
